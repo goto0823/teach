652ab2bbddf2c0d35ab77eaf2d82406a7e026a1e
f.goto <you@example.com>
2025-04-04 13:31:55 +0000

    create_table_view

---
+++ resources/views/Layouts/header.blade.php
@@ -0,0 +1,13 @@
+<header class="w-full lg:max-w-4xl h-[50px] pt-[15px] text-sm mb-6 not-has-[nav]:hidden  bg-violet-300">
+    <nav class="flex items-center justify-end gap-4 list-none mx-10">
+        <li>
+            料理一覧
+        </li>
+        <li>
+            材料一覧
+        </li>
+        <li>
+            注文をする
+        </li>
+    </nav>
+</header>
