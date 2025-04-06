dc6ee9b7bba0a80508f96fb91a24d382e956b061
f.goto <f.goto@r102.co.jp>
2025-04-06 23:34:59 +0000

    add_test

--- resources/views/Layouts/header.blade.php
+++ resources/views/Layouts/header.blade.php
@@ -1,5 +1,8 @@
-<header class="w-full lg:max-w-4xl h-[50px] pt-[15px] text-sm mb-6 not-has-[nav]:hidden  bg-violet-300">
-    <nav class="flex items-center justify-end gap-4 list-none mx-10">
+<header class="w-full h-[50px] pt-[15px] text-sm mb-6 not-has-[nav]:hidden  bg-violet-300">
+    <nav 
+        class="flex items-center justify-end gap-4 list-none mx-10"
+        aria-label="header-nav"    
+    >
         <li>
             料理一覧
         </li>
